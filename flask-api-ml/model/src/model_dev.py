74dd4ec03f8f5467c45d8aae68a146b8172b1fc1
benjamincrt <azertyben2000@orange.fr>
2024-02-08 02:32:55 +0100

    init

---
+++ flask-api-ml/model/src/model_dev.py
@@ -0,0 +1,24 @@
+from sklearn.linear_model import LinearRegression
+import pickle5 as pickle
+import os
+
+class ModelDevelopment:
+    def __init__(self):
+        self.model = LinearRegression()
+
+    def train(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def save_model(self, model_directory):
+        if not os.path.exists(model_directory):
+            os.makedirs(model_directory)
+        model_path = os.path.join(model_directory, 'trained_model.pkl')
+        with open(model_path, 'wb') as f:
+            pickle.dump(self.model, f)
+        return model_path
+    
+    def load_model(self, model_path):
+        # Méthode pour charger un modèle préalablement entraîné
+        with open(model_path + "/trained_model.pkl", 'rb') as f:
+            model = pickle.load(f)
+        return model
