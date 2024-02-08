a0a045b3eb3f4a7be7159b5a6003a7502fbaa0d8
benjamincrt <azertyben2000@orange.fr>
2024-02-08 22:20:23 +0100

    add 2 routes for pred & retrain

--- flask-api-ml/app.py
+++ flask-api-ml/app.py
@@ -1,65 +1,41 @@
 from flask import Flask, request, render_template, redirect, url_for
-from model.src.model_dev import ModelDevelopment
+import pandas as pd
 
 app = Flask(__name__)
 
-class PredictionParams:
-    def __init__(self, age, sex, bmi, children, smoker, region):
-        self.age = age,
-        self.sex = sex,
-        self.bmi = bmi,
-        self.children = children,
-        self.smoker = smoker,
-        self.region = region
-    
-    def prediction(self):
-        model = ModelDevelopment().load_model("model/saved_model/trained_model.pkl")
-
-        return f"{self.age} / {self.sex} / {self.bmi} / {self.children} / {self.smoker} / {self.region} == "
-
-# In-memory database
-predictions = []
-
-@app.route('/')
-def index():
-    return render_template('index.html', items=predictions)
-
-@app.route('/predict', methods=['POST'])
+# Take list of args in params
+@app.route('/predict', methods=['GET'])
 def predict():
-    age = request.form.get('age')
-    sex = request.form.get('sex')
-    bmi = request.form.get('bmi')
-    children = request.form.get('children')
-    smoker = request.form.get('smoker')
-    region = request.form.get('region')
-    if age & sex & bmi & children & smoker & region:
-        params = PredictionParams(age, sex, bmi, children, smoker, region)
-        predictions.append(params)
-    return redirect(url_for('index'))
+    try:
+        params = {
+            "age": request.args.get('age'),
+            "sex": request.args.get('sex'),
+            "bmi": request.args.get('bmi'),
+            "children": request.args.get('children'),
+            "smoker": request.args.get('smoker'),
+            "region": request.args.get('region')
+        }
 
+        # prediction = subprocess(....) (pipe)
+        
+        return params, 200
+    except Exception as e:
+        raise f"[ERROR] predict request: {e}"
 
-@app.route('/get')
-def get():
-    return predictions
-"""
-@app.route('/add', methods=['POST'])
-def add_item():
-    item = request.form.get('item')
-    if item:
-        items.append(item)
-    return redirect(url_for('index'))
+# Take json object in params  
+@app.route('/retrain', methods=['POST'])
+def retrain():
+    try:
+        data = request.json
+        df = pd.DataFrame(data)
 
-@app.route('/delete/<int:index>')
-def delete_item(index):
-    if index < len(items):
-        items.pop(index)
-    return redirect(url_for('index'))
+        #jsons = df.to_json(index=False) ... to convert data in json
 
-@app.route('/update/<int:index>', methods=['POST'])
-def update_item(index):
-    if index < len(items):
-        items[index] = request.form.get('new_item')
-    return redirect(url_for('index'))
-"""
+        #new_score = .... (pipe)
+        
+        return jsons, 200
+    except Exception as e:
+        raise f"[ERROR] retrain request: {e}"
+    
 if __name__ == '__main__':
     app.run(debug=True)
